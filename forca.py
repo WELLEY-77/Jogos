e95257836846ea2a5925a99976cea3b1cf3643ea
Welley <monteirowelley7@gmail.com>
2022-10-22 14:56:46 -0300

    add while  and for

--- forca.py
+++ forca.py
@@ -3,11 +3,23 @@ def jogar_forca():
     print('BEM VINDO AO JOGO DA FORCA')
     print('*'*26)
     
+    palavra_secreta = 'banana'
+
     acertou = False
     enforcou = False
 
     while(not enforcou and not acertou):
+
+        chute = input('Digite uma letra: ')
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute == letra):
+                print('Encontrei a letra {} na posição {} '.format(chute,index))
+            index = index + 1
         print('Jogando...')
+    
+    print('FIM DE JOGO')
 
 if (__name__ == '__main__'):
     jogar_forca()
