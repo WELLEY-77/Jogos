e8253b39ac2592d8140129f65736add71e40e76f
Welley <monteirowelley7@gmail.com>
2022-10-22 14:18:19 -0300

    forca em criação, add while

--- forca.py
+++ forca.py
@@ -3,5 +3,11 @@ def jogar_forca():
     print('BEM VINDO AO JOGO DA FORCA')
     print('*'*26)
     
+    acertou = False
+    enforcou = False
+
+    while(not enforcou and not acertou):
+        print('Jogando...')
+
 if (__name__ == '__main__'):
     jogar_forca()
