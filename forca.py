52a74d9c49dd01e3c535e03ce366cd523b24ec28
Welley <monteirowelley7@gmail.com>
2022-11-05 13:34:25 -0300

    adicionando o total de tentativas que pode errar

--- forca.py
+++ forca.py
@@ -3,26 +3,31 @@ def jogar_forca():
     print('BEM VINDO AO JOGO DA FORCA')
     print('*'*26)
     
-    palavra_secreta = 'banana'
+    palavra_secreta = 'banana'.upper()
     letra_secreta = ['_', '_', '_', '_', '_', '_']
 
     acertou = False
     enforcou = False
+    tentativas = 0
 
     while(not enforcou and not acertou):
 
-        chute = input('Digite uma letra: ').strip()
+        chute = input('Digite uma letra: ').strip().upper()
         
         #---------Pode fazer dessa outra maneira tambem para tira os espaços
         #chute = chute.strip()
-
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letra_secreta[index] = letra  
-            index = index + 1
-            
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letra_secreta[index] = letra  
+                index = index + 1
+        else:
+            tentativas += 1
+        
+        enforcou = tentativas == 6
         print(letra_secreta)
+            
     print('FIM DE JOGO')
 
 if (__name__ == '__main__'):
